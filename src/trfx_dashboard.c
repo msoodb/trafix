df091cb3b6559f45b33d309019da40f85178bfba
msoodb <masoud.bolhassani@gmail.com>
2025-04-22 16:16:08 +0300

    compact process view

--- src/trfx_dashboard.c
+++ src/trfx_dashboard.c
@@ -105,6 +105,7 @@ void *cpu_info_thread(void *arg) {
 
     int h, w;
     getmaxyx(win, h, w);
+    (void)w;
 
     // Apply color before drawing border
     wattron(win, COLOR_PAIR(COLOR_BORDER));
@@ -153,10 +154,6 @@ void *cpu_info_thread(void *arg) {
 
       row++;
     }
-
-    // Per-core data
-    int cores_per_row = (w - line) / 25;
-    int core_count = 0;
 
     char filled_char = '=';
     char empty_char = ' ';
@@ -337,7 +334,6 @@ void *process_info_thread(void *arg) {
     pthread_mutex_lock(&ncurses_mutex);
     int h, w;
     getmaxyx(win, h, w);
-
     werase(win);
 
     wattron(win, COLOR_PAIR(COLOR_BORDER));
@@ -358,40 +354,59 @@ void *process_info_thread(void *arg) {
     int count = get_top_processes_by_mem(list, MAX_PROCESSES);
 
     pthread_mutex_lock(&ncurses_mutex);
-    
-    int row = 1;
 
-    const char *header =
-      "  PID    USER      PR  NI    VIRT    RES      SHR S   %%CPU %%MEM   TIME+     COMMAND               ";
-    char clipped_header[1024];
-    strncpy(clipped_header, header, w - 2);
-    clipped_header[w - 2] = '\0';
+    int row = 0;
 
-    wattron(win, COLOR_PAIR(COLOR_HEADER));
-    mvwprintw(win, row++, 1, "%s", clipped_header);
-    wattroff(win, COLOR_PAIR(COLOR_HEADER));
+    wattron(win, A_BOLD);
+    mvwprintw(win, row++, 2, " Process Information ");
+    wattroff(win, A_BOLD);
 
+    int compact_mode = 1;  // 0 = full, 1 = compact
     int max_rows = h - 2;
 
-    for (int i = 0; i < count && row < max_rows; i++) {
-      char line[1024];
-      snprintf(line, sizeof(line),
-               "%7.7s %-10.10s %2.2s %2.2s %8.8s %7.7s %7.7s %1.1s %5.5s %5.5s %10.10s %-20.20s",
-               list[i].pid,
-               list[i].user,
-               list[i].pr,
-               list[i].ni,
-               list[i].virt,
-               list[i].res,
-               list[i].shr,
-               list[i].state,
-               list[i].cpu,
-               list[i].mem,
-               list[i].time,
-               list[i].command);
+    if (compact_mode) {
+      const char *header = "  PID    USER        %CPU  %MEM   COMMAND";
+      char clipped_header[1024];
+      strncpy(clipped_header, header, w - 2);
+      clipped_header[w - 2] = '\0';
 
-      line[w - 2] = '\0';
-      mvwprintw(win, row++, 1, "%s", line);
+      wattron(win, COLOR_PAIR(COLOR_HEADER));
+      mvwprintw(win, row++, 1, "%s", clipped_header);
+      wattroff(win, COLOR_PAIR(COLOR_HEADER));
+
+      for (int i = 0; i < count && row < max_rows; i++) {
+        char line[1024];
+        snprintf(line, sizeof(line), "%7.7s %-10.10s %5.5s %5.5s %-20.20s",
+                 list[i].pid, list[i].user, list[i].cpu, list[i].mem,
+                 list[i].command);
+
+        line[w - 2] = '\0';
+        mvwprintw(win, row++, 1, "%s", line);
+      }
+
+    } else {
+      const char *header = "  PID    USER      PR  NI    VIRT    RES      SHR "
+                           "S   %%CPU %%MEM   TIME+     COMMAND               ";
+      char clipped_header[1024];
+      strncpy(clipped_header, header, w - 2);
+      clipped_header[w - 2] = '\0';
+
+      wattron(win, COLOR_PAIR(COLOR_HEADER));
+      mvwprintw(win, row++, 1, "%s", clipped_header);
+      wattroff(win, COLOR_PAIR(COLOR_HEADER));
+
+      for (int i = 0; i < count && row < max_rows; i++) {
+        char line[1024];
+        snprintf(line, sizeof(line),
+                 "%7.7s %-10.10s %2.2s %2.2s %8.8s %7.7s %7.7s %1.1s %5.5s "
+                 "%5.5s %10.10s %-20.20s",
+                 list[i].pid, list[i].user, list[i].pr, list[i].ni,
+                 list[i].virt, list[i].res, list[i].shr, list[i].state,
+                 list[i].cpu, list[i].mem, list[i].time, list[i].command);
+
+        line[w - 2] = '\0';
+        mvwprintw(win, row++, 1, "%s", line);
+      }
     }
 
     wrefresh(win);
@@ -506,6 +521,7 @@ void *network_info_thread(void *arg) {
     pthread_mutex_lock(&ncurses_mutex);
 
     getmaxyx(win, max_rows, max_cols);
+    (void)max_cols;
     int row = 0;
     int line = 4;
     int max_lines = max_rows - 1;
@@ -600,9 +616,10 @@ void start_dashboard() {
   };
 
   // Row 2 widths (2 columns)
-  int row2_widths[2] = {
-    screen_width / 2,
-    screen_width - (screen_width / 2)
+  int row2_widths[3] = {
+    screen_width / 3,
+    screen_width / 3,
+    screen_width - ((int)(screen_width / 3) + (int)(screen_width / 3))
   };
 
   // Y offsets
@@ -615,33 +632,30 @@ void start_dashboard() {
   WINDOW *mem_win = newwin(row1_height, row1_widths[2], row1_y, row1_widths[0] + row1_widths[1]);
   WINDOW *disk_win = newwin(row1_height, row1_widths[3], row1_y,row1_widths[0] + row1_widths[1] + row1_widths[2]);
 
-  // Create row 2 windows
-  WINDOW *proc_win = newwin(row2_height, row2_widths[0], row2_y, 0);
+  // Create row 2 windows  
+  WINDOW *conn_win = newwin(row2_height, row2_widths[0], row2_y, 0);
   WINDOW *net_win = newwin(row2_height, row2_widths[1], row2_y, row2_widths[0]);
-  //WINDOW *conn_win = newwin(row2_height, row2_widths[1], row2_y, row2_widths[0]);
+  WINDOW *proc_win = newwin(row2_height, row2_widths[2], row2_y, row2_widths[0] + row2_widths[1]);
+  
  
   // Launch threads
   pthread_t sys_tid;
   pthread_t cpu_tid;
   pthread_t mem_tid;
-  pthread_t disk_tid;
-  pthread_t proc_tid;
+  pthread_t disk_tid;  
   pthread_t net_tid;
   pthread_t conn_tid;
+  pthread_t proc_tid;
   
   pthread_create(&sys_tid, NULL, system_info_thread, sys_win);
   pthread_create(&cpu_tid, NULL, cpu_info_thread, cpu_win);
   pthread_create(&mem_tid, NULL, memory_info_thread, mem_win);
   pthread_create(&disk_tid, NULL, disk_info_thread, disk_win);
 
-  pthread_create(&proc_tid, NULL, process_info_thread, proc_win);
+  pthread_create(&conn_tid, NULL, connection_info_thread, conn_win);
   pthread_create(&net_tid, NULL, network_info_thread, net_win);
-  //pthread_create(&vpn_tid, NULL, vpn_info_thread, vpn_win);
-
-  //pthread_create(&conn_tid, NULL, connection_info_thread, conn_win);
-  //pthread_create(&log_tid, NULL, logs_info_thread, log_win);
-
-  // Signal ready and keep UI alive
+  pthread_create(&proc_tid, NULL, process_info_thread, proc_win);
+  
   sleep(1);
   ready = 1;
 
