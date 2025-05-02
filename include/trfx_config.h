41d2a7f89b5fd85237ffcf2e45562d809252a24f
msoodb <masoud.bolhassani@gmail.com>
2025-05-02 20:17:11 +0300

    config file path

--- include/trfx_config.h
+++ include/trfx_config.h
@@ -10,11 +10,10 @@
 #ifndef TRFX_CONFIG_H
 #define TRFX_CONFIG_H
 
-#define CONFIG_FILE "config/config.cfg"
+#define CONFIG_FILE "/etc/trafix/config.cfg"
 
-extern int alert_bandwidth;
-extern char filter_ip[16];
-extern char filter_process[50];
+extern int temp_warn_red;
+extern int temp_warn_red;
 
 void read_config(const char *config_file);
 
