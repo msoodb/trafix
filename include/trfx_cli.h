3667562b4693c3e3c4a604ea3a35f9762b904ffb
msoodb <masoud.bolhassani@gmail.com>
2026-05-27 15:52:56 +0300

    Create a small struct that represents parsed command-line intent.

---
+++ include/trfx_cli.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 Masoud Bolhassani <masoud.bolhassani@gmail.com>
+ *
+ * This file is part of Trafix.
+ *
+ * Trafix is released under the GNU General Public License v3 (GPL-3.0).
+ * See LICENSE file for details.
+ */
+
+#ifndef TRFX_CLI_H
+#define TRFX_CLI_H
+
+#define TRFX_CLI_ERROR_MAX 128
+
+typedef enum {
+  TRFX_CLI_MODE_TUI = 0,
+  TRFX_CLI_MODE_HELP,
+  TRFX_CLI_MODE_VERSION,
+  TRFX_CLI_MODE_INVALID
+} TrfxCliMode;
+
+typedef struct {
+  TrfxCliMode mode;
+  char error[TRFX_CLI_ERROR_MAX];
+} TrfxCliOptions;
+
+#endif // TRFX_CLI_H
---
+++ src/trfx_cli.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2025 Masoud Bolhassani <masoud.bolhassani@gmail.com>
+ *
+ * This file is part of Trafix.
+ *
+ * Trafix is released under the GNU General Public License v3 (GPL-3.0).
+ * See LICENSE file for details.
+ */
+
+#include "trfx_cli.h"
